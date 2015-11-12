d04e07b954116bf060ae694babcf4b1f6b9aa480
Hallvord R. M. Steen <hsteen@mozilla.com>
2015-11-12 13:04:46 +0100

    Some initial work on tests, more TBD

---
+++ test/api/onconsolemessage.py
@@ -0,0 +1,124 @@
+import json
+import os
+import requests
+import sys
+import time
+import unittest
+
+# URL of SlimerJS / PhantomJS server
+url = os.environ['COMPATIPEDE_TAB_URL']
+
+def do_command(url, command, data={}):
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    response = requests.post('%s/%s' % (url, command),
+                             data=json.dumps(data), headers=headers)
+    if response.status_code == 500:
+        do_command(url, "destroy")
+        sys.exit(1)
+    return response
+
+class TestConsoleLogging(unittest.TestCase):
+
+    # Note: the order matters. This API reports *accumulated* messages
+    # during the session. Hence, the tests expected to be no-ops must be first
+
+    def test_logging_empty(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>void(\'foo\')</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 0)
+
+
+    def test_logging_compile(self):
+        # Compile-time and run-time errors will not be reported here
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>void(\'foo)</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 0)
+
+
+    def test_logging_runtime(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>undefined(\'foo\')</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 0)
+
+
+    def test_console_log(self):
+        # Test the various console.*() methods
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>console.log(\'foo\')</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 1)
+        self.assertEqual(data['consoleLog'][0]['msg'], 'foo')
+        self.assertEqual(data['consoleLog'][0]['lineNum'], 1)
+
+
+    def test_console_info(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>console.info(\'foo\')</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 1)
+        self.assertEqual(data['consoleLog'][0]['msg'], 'foo')
+        self.assertEqual(data['consoleLog'][0]['lineNum'], 1)
+
+
+    def test_console_warn(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>console.warn(\'foo\')</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 1)
+        self.assertEqual(data['consoleLog'][0]['msg'], 'foo')
+        self.assertEqual(data['consoleLog'][0]['lineNum'], 1)
+
+
+    def test_console_error(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>console.error(\'foo\')</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getConsoleLog")
+
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('consoleLog' in data)
+        self.assertEqual(len(data['consoleLog']), 1)
+        self.assertEqual(data['consoleLog'][0]['msg'], 'foo')
+        self.assertEqual(data['consoleLog'][0]['lineNum'], 1)
+
+    @classmethod
+    def tearDownClass(self):
+        # Destroy Tab
+        response = do_command(url, "destroy")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/plugins/window-orientation.py
@@ -0,0 +1,52 @@
+import json
+import os
+import requests
+import sys
+import time
+import unittest
+
+# URL of SlimerJS / PhantomJS server
+url = os.environ['COMPATIPEDE_TAB_URL']
+
+def do_command(url, command, data={}):
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    response = requests.post('%s/%s' % (url, command),
+                             data=json.dumps(data), headers=headers)
+    if response.status_code == 500:
+        do_command(url, "destroy")
+        sys.exit(1)
+    return response
+
+class TestConsoleLogging(unittest.TestCase):
+
+    # Note: the order matters. This API reports *accumulated* messages
+    # during the session. Hence, the tests expected to be no-ops must be first
+
+    def test_no_orientation_getter(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>void(\'foo\');</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getPluginsResults")
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('results' in data)
+        self.assertEqual(data['results']['window-orientation-usage'], False)
+
+    def test_orientation_getter(self):
+        # Open URL
+        data = {'url': 'data:text/html,<html><script>void(window.orientation);</script><p>Hello test</p></html>'}
+        response = do_command(url, "open", data)
+
+        response = do_command(url, "getPluginsResults")
+        data = json.loads(response.text.encode('utf-8').strip())
+        self.assertTrue('results' in data)
+        self.assertEqual(data['results']['window-orientation-usage'], True)
+
+
+    @classmethod
+    def tearDownClass(self):
+        # Destroy Tab
+        response = do_command(url, "destroy")
+
+if __name__ == '__main__':
+    unittest.main()
